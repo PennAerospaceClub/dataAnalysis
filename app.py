99f1cb3d3ca888315e6b936c924098f7635d4157
nsobotka <90986529+nsobotka@users.noreply.github.com>
2022-11-19 15:21:14 -0500

    uploading csv frontend

--- app.py
+++ app.py
@@ -1,15 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from werkzeug.utils import secure_filename
+import os
 import pandas as pd
 import json
 import plotly
 import plotly.express as px
-from .scripts.flightPath import fp
+# from .scripts.flightPath import fp
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join('data', 'uploads')
+ALLOWED_EXTENSIONS = {'csv'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.secret_key = 'pachab'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('plot.html')
+
+@app.route('/upload', methods=("POST", "GET"))
+def uploadFile():
+    if request.method == 'POST':
+        uploaded_df = request.files['uploaded-file']
+        data_filename = secure_filename(uploaded_df.filename)
+        uploaded_df.save(os.path.join(app.config['UPLOAD_FOLDER'], data_filename))
+
+        session['u_data'] = os.path.join(app.config['UPLOAD_FOLDER'], data_filename)
+
+        # get and parse csv
+        data_file_path = session.get('u_data', None)
+
+        uploaded_df = pd.read_csv(data_file_path)
+
+        uploaded_df_html = uploaded_df.to_html()
+
+        ## include df in render_template
+
+        return render_template('plot.html')
 
 @app.route('/demo')
 def demo():
@@ -37,4 +69,4 @@ def demo():
     description = """
     A 3-d visualization of the flight path based on data collected from a high-altitude balloon.
     """
-    return render_template('plot.html', launchSite=fp.getMaxCol() graphJSON=graphJSON, header=header,description=description)
+    return render_template('plot.html', graphJSON=graphJSON, header=header,description=description)
